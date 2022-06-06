6a15c0498c77379175231e12fa0295bbb7b76a1d
Alwin Ang <alwinang99@gmail.com>
2022-06-06 20:56:24 +0800

    write tests

--- car_factory.py
+++ car_factory.py
@@ -13,7 +13,7 @@ class CarFactory():
         return car
 
     def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
-        engine = WilloughbyEngine(last_service_date, current_mileage)
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
         battery = SpindlerBattery(last_service_date, current_date)
         car = Car(engine, battery)
         return car        
@@ -25,7 +25,7 @@ class CarFactory():
         return car    
 
     def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage):
-        engine = WilloughbyEngine(last_service_date, current_mileage)
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
         battery = NubbinBattery(last_service_date, current_date)
         car = Car(engine, battery)
         return car     
